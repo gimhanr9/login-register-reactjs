ff68b4b98f0c73b29b9e2e359923efefce4558a2
Gimhan Rodrigo <gimhanrg@gmail.com>
2021-07-05 14:01:03 +0530

    Password hashing features enabled

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mysql = require('mysql');
+const bcrypt = require('bcryptjs');
 
 const db = mysql.createPool({
     host: 'localhost',
@@ -19,16 +20,22 @@ app.get("/", (req, res) => {
 
 });
 
-app.post("/login", (req, res) => {
+app.post("/login", async(req, res) => {
     const email = req.body.email;
     const password = req.body.password;
-    const sqlCheck = "SELECT * from user WHERE Email=? AND Password=?"
-    db.query(sqlCheck, [email, password], (err, result) => {
+
+    const sqlCheck = "SELECT * from user WHERE Email=?"
+    db.query(sqlCheck, [email], (err, result) => {
         if (err) {
             res.send({ error: err });
         }
         if (result.length > 0) {
-            res.send(result);
+            const compare = await bcrypt.compare(password, result[0].Password);
+            if (compare) {
+                res.send(result);
+            } else {
+                res.send({ message: "Invalid Email or Password!" });
+            }
         } else {
             res.send({ message: "Invalid Email or Password!" });
         }
@@ -36,13 +43,14 @@ app.post("/login", (req, res) => {
 
 });
 
-app.post("/register", (req, res) => {
+app.post("/register", async(req, res) => {
     const userName = req.body.userName;
     const email = req.body.email;
     const password = req.body.password;
+    const hashedPassword = await bcrypt.hash(password, 10);
     const phone = req.body.phone;
     const sqlIns = "INSERT INTO user(Name,Email,Password,Phone) VALUES (?,?,?,?)";
-    db.query(sqlIns, [userName, email, password, phone], (err, result) => {
+    db.query(sqlIns, [userName, email, hashedPassword, phone], (err, result) => {
 
     });
 
